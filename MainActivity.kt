4bba4c0dd88c5e41bff0cf4f1ce8aa7076c9cb38
Raad Fahd Abdo Qaid Al-Selwi <r.dalselwi@gmail.com>
2026-09-19 03:16:18 +0300

    Delete MainActivity.kt

--- MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.corolla_control
-
-// 🔐 استخدام الـ FragmentActivity لتهيئة نافذة البصمة بنجاح ومنع الانهيار
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity() {
-    // يترك فارغاً، التغيير فقط في نوع الوراثة الفوقية لدعم نظام التبصيم
-}
