d4ee73b655e5cfeafbb073fe2115646638443dc9
Raad Fahd Abdo Qaid Al-Selwi <r.dalselwi@gmail.com>
2026-09-20 15:36:49 +0300

    Add files via upload

---
+++ android/app/src/main/java/io/flutter/plugins/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.corolla_control
+
+// 🔐 استخدام الـ FragmentActivity لتهيئة نافذة البصمة بنجاح ومنع الانهيار
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity() {
+    // يترك فارغاً، التغيير فقط في نوع الوراثة الفوقية لدعم نظام التبصيم
+}
